bec4eb5945612a11356b8ec92157cd03226037f4
Ihlaikin Mykola <Ggg501434@gmail.com>
2025-05-28 19:23:52 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "Lab2-Unit-Tests",
+    products: [
+        .executable(
+            name: "Lab2-Unit-Tests",
+            targets: ["Lab2-Unit-Tests"]
+        ),
+        .library(
+            name: "CustomListCore",
+            targets: ["CustomListCore"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "CustomListCore",
+            path: "Sources/CustomListCore"
+        ),
+        .executableTarget(
+            name: "Lab2-Unit-Tests",
+            dependencies: ["CustomListCore"],
+            path: "Sources",
+            exclude: ["CustomListCore"]         
+        ),
+        .testTarget(
+            name: "CustomListTests",
+            dependencies: ["CustomListCore"]
+        ),
+    ]
+)
